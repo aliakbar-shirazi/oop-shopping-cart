57fa065c4d7f2c2c2f4e1bd98d1cb15f6b5c75ea
Ali Akbar Shirazi <ali.shirazi.dev@gmail.com>
2025-08-22 22:44:43 +0330

    feat(cart): add shopping cart and product-cart integration
    - create 'models/cart.js' with OOP structure for shopping cart
    - integrate cart instance in 'app.js'
    - add function in 'models/products.js' to accept cart instance

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import { fetchData } from "./utils/httpreq.js";
 import Products from "./models/products.js";
+import Cart from "./models/cart.js";
 
 const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document.querySelector("#total-price span");
 
 async function render() {
   const productsData = await fetchData();
-  const productsInstance = new Products(productsNode, productsData);
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productsInstance = new Products(
+    productsNode,
+    productsData,
+    cartInstance
+  );
   productsInstance.showProducts();
 }
 
